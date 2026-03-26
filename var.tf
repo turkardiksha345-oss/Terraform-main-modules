f36983a6fc5de48e58415e7998543f2d700200a7
Diksha Turkar <turkardiksha345@gmail.com>
2026-03-26 10:47:59 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,6 +2,7 @@
 variable "vpc_cidr_block" {  }
 variable "public_subnet_cidr" {  }
 variable "private_subnet_cidr" {  }
+variable "private_subnet_cidr_2" {  }
 variable "public_az" { }
 variable "private_az" {  }
 
